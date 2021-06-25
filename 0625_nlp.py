f29c4c8f3ba7c6e10d5754ed596fef61773109c1
HC-kang <weston0713@gmail.com>
2021-06-25 14:57:15 +0900

    0625

---
+++ 0625_nlp.py
@@ -0,0 +1,105 @@
+from nltk import tokenize
+from nltk.text import TokenSearcher
+import numpy as np
+import pandas as pd
+
+from nltk.corpus import stopwords
+import string 
+import nltk
+from tensorflow.python.keras.utils.np_utils import to_categorical
+
+nltk.download('stopwords')
+
+mail = pd.DataFrame(columns = ['num', 'label', 'text', 'label_num'])
+mail['num'] = [1,2,3,4,5,6]
+mail['label'] = ['spam', 'spam', 'ham', 'ham', 'ham', 'spam']
+mail['text'] = ['your free lottery', 'free lottery free you', 'your free apple', 'free to contact me', 'I won award', 'my lottery ticket']
+mail['label_num'] = [1,1,0,0,0,1]
+mail
+
+
+def process_text(text):
+  # text에서 구두점(punctuation) 제거하기
+  nopunc = [char for char in text if char not in string.punctuation]
+  nopunc = ''.join(nopunc)
+  # text에서 불용어 제거
+  cleaned_words = [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+
+  return cleaned_words
+
+
+mail['text'].apply(process_text)
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+msg_bow = CountVectorizer(analyzer= process_text).fit_transform(mail['text'])
+
+
+# data split(train:8, test:2)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(msg_bow, mail['label_num'], test_size = 0.2, random_state=42)
+
+
+# 다항식 나이브 베이즈 모델 훈련
+from sklearn.naive_bayes import MultinomialNB
+classifier = MultinomialNB()
+classifier.fit(X_train, y_train)
+
+classifier.predict(X_test)
+y_test.values
+
+classifier.predict(X_train)
+y_train.values
+
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+train_pred = classifier.predict(X_train) # 모델에 관한 예측값 출력
+test_pred = classifier.predict(X_test) 
+
+print(classification_report(y_train, train_pred))
+print(classification_report(y_test, test_pred))
+
+confusion_matrix(y_train, train_pred)
+confusion_matrix(y_test, test_pred)
+
+accuracy_score(y_train, train_pred)
+accuracy_score(y_test, test_pred)
+
+from konlpy.tag import *
+okt = Okt()
+
+
+token = okt.morphs('나는 자연어를 공부한다')
+token
+
+word2idx={}
+
+for voca in token:
+    if voca not in word2idx.keys():
+        word2idx[voca] = len(word2idx)
+
+print(word2idx)
+
+def one_hot(word, word2idx):
+    one_hot_vector = [0]*(len(word2idx))
+    index = word2idx[word]
+    one_hot_vector[index] = 1
+    return one_hot_vector
+
+one_hot('공부', word2idx)
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+text = "나랑 점심 먹으러 갈래 점심 메뉴는 햄버거 갈래 갈래 햄버거 최고야"
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts([text])
+print(tokenizer.word_index)
+
+sub_text = '점심 먹으러 갈래 메뉴는 햄버거 최고야'
+encoded = tokenizer.texts_to_sequences([sub_text])[0]
+print(encoded)
+
+one_hot = to_categorical(encoded)
+print(one_hot)
+.tag
---
+++ 0625_nlp2.py
@@ -0,0 +1,285 @@
+from konlpy.tag import Okt
+import re
+
+okt = Okt()
+token = re.sub('(\.)', '', '정부가 발표하는 물가상승률과 소비자가 느끼는 물가상승률은 다르다')
+
+token
+
+okt.morphs(token)
+
+token = okt.morphs(token)
+token
+
+word2idx = {} 
+bow = [] # bag of words
+
+for voca in token: # 토큰화 된 단어를 하나씩 불러오기
+    if voca not in word2idx.keys(): # 단어사전에 voca가 없다면
+        word2idx[voca] = len(word2idx) # 단어사전의 길이 len을 해당 voca의 value로 설정 - 순서 기입
+        bow.insert(len(word2idx)-1, 1)
+        # bow에 횟수를 1로 초기화
+    else: # 만약 있으면
+        index = word2idx.get(voca) # 해당 voca의 value값을 가져오기
+        bow[index] = bow[index]+1 # value 값을 index로 bow에서 꺼내와서, 1 더하기.
+
+print(word2idx)
+print(bow)
+
+# countervectorizer 이용
+from sklearn.feature_extraction.text import CountVectorizer
+corpus = ['you know i want your love. because I love you.']
+vector = CountVectorizer()
+print(vector.fit_transform(corpus).toarray())
+print(vector.vocabulary_)
+
+
+
+################################################################################
+
+from math import *
+import pandas as pd
+
+docs = [
+  '먹고 싶은 사과',
+  '먹고 싶은 바나나',
+  '길고 노란 바나나 바나나',
+  '저는 과일이 좋아요'
+]
+
+voca = list(set(w for doc in docs for w in doc.split()))
+voca
+voca.sort()
+
+print(voca)
+
+len(docs)
+
+N = len(docs)
+
+def tf(t, d): # term frequency : 단어의 빈도
+    return d.count(t)
+
+def idf(t): # inversed document frequency : 역 문서 빈도
+    df = 0
+    for doc in docs:
+        df += t in doc
+    return log(N/(df+1))
+
+def tfidf(t, d):
+    return tf(t,d) * idf(t)
+
+# term frequency
+result = []
+for i in range(N):
+    result.append([])
+    d = docs[i]
+    for j in range(len(voca)):
+        t = voca[j]
+        result[-1].append(tf(t, d))
+
+tf_ = pd.DataFrame(result, columns = voca)
+tf_
+
+# idf
+result = []
+for j in range(len(voca)):
+    t = voca[j]
+    result.append(idf(t))
+
+idf_ = pd.DataFrame(result, index=voca, columns = ['IDF'])
+idf_
+
+# tf - idf
+result = []
+for i in range(N):
+    result.append([])
+    d = docs[i]
+    for j in range(len(voca)):
+        t = voca[j]
+
+        result[-1].append(tfidf(t, d))
+
+tfidf_ = pd.DataFrame(result, columns = voca)
+tfidf_
+
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+texts = ['먹고 싶은 사과', '먹고 싶은 바나나', '길고 노란 바나나 바나나', '저는 과일이 좋아요']
+token = Tokenizer()
+token.fit_on_texts(texts)
+print(token.word_index)
+
+print(token.texts_to_matrix(texts, mode = 'count'))
+print(token.texts_to_matrix(texts, mode = 'binary'))
+
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
+
+'''
+딥러닝 실행 순서
+1. 데이터 수집 - 전처리
+2. 모델링
+3. 컴파일 : 환경설정 (train data)
+4. 학습 (fit, fit_transform)
+'''
+
+import tensorflow as tf
+tf.keras.layers.Dense(10)
+
+from tensorflow.keras.layers import Dense
+Dense(10)
+
+import numpy as np
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
+# 데이터 전처리
+xs = np.array([-1., 0., 1., 2., 3., 4.], dtype = float)
+ys = np.array([5., 6., 7., 8. ,9. ,10.], dtype = float)
+
+# 모델링
+model = Sequential()
+model.add(Dense(1, input_dim = 1, activation = 'linear'))
+model.compile(optimizer='sgd', loss = 'mse')
+
+# 학습
+model.fit(xs,ys, epochs = 1200, verbose = 0)
+
+model.predict([10.0])
+
+# 선형회귀 구현하기
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import optimizers
+from tensorflow.keras.optimizers import Adam
+import numpy as np
+
+X=[1,2,3,4,5,6,7,8,9] # 공부하는 시간
+y=[11,22,33,44,53,66,77,87,95] # 각 공부하는 시간에 맵핑되는 성적
+
+model = Sequential()
+model.add(Dense(1, input_dim = 1, activation='linear'))
+sgd = optimizers.SGD(lr=0.01)
+
+model.compile(optimizer = sgd, loss = 'mse', metrics = 'accuracy')
+model.fit(X, y, batch_size = 32, shuffle =False, epochs = 300)
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+ax.plot(X, model.predict(X), 'b', X, y, 'k.')
+ax.plot(X, y, 'r', X, y, 'k.')
+
+import tensorflow as tf
+import numpy as np
+
+w = tf.Variable(2.)
+
+def f(w):
+    y = w**2
+    z = 2*y + 5
+    return z
+
+with tf.GradientTape() as tape:
+    z = f(w)
+
+X=[1,2,3,4,5,6,7,8,9] # 공부하는 시간
+y=[11,22,33,44,53,66,77,87,95] # 각 공부하는 시간에 맵핑되는 성적
+
+W = tf.Variable(4.)
+b = tf.Variable(1.)
+
+@tf.function
+def hypothesis(x):
+    return W*x+b
+
+x_test = [3.5, 5, 5.5, 6]
+
+print(hypothesis(x_test).numpy())
+
+@tf.function
+def mse_loss(y_pred, y):
+    return tf.reduce_mean(tf.square(y_pred-y))
+
+optimizer = tf.optimizers.SGD(lr=0.01)
+for i in range(301):
+    with tf.GradientTape() as tape:
+        y_pred = hypothesis(X)
+        cost = mse_loss(y_pred, y)
+
+    gradients = tape.gradient(cost, [W, b])
+    
+    optimizer.apply_gradients(zip(gradients, [W,b]))
+
+    if i%10 ==0:
+        print('epoch : {:3} | W의 값 : {:5.4f} | b의 값 : {:5.4} | cost : {:5.6f}'.format(i, W.numpy(), b.numpy(), cost))
+
+x_test = [3.5, 5, 5.5, 6, 9.5]
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+x = np.arange(-5, 5, 0.1)
+y = sigmoid(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y, 'g')
+ax.plot([0,0], [1.0, 0.0], ':')
+
+plt.plot(x, y, 'g')
+plt.plot([0,0], [1.0, 0.0], ':')
+plt.title('sigmoid')
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+x = np.arange(-5.0, 5.0, 0.1)
+y1 = sigmoid(0.5*x)
+y2 = sigmoid(x)
+y3 = sigmoid(2*x)
+
+plt.plot(x, y1, 'r', linestyle='--') # W의 값이 0.5일때
+plt.plot(x, y2, 'g') # W의 값이 1일때
+plt.plot(x, y3, 'b', linestyle='--') # W의 값이 2일때
+plt.plot([0,0],[1.0,0.0], ':') # 가운데 점선 추가
+plt.title('Sigmoid Function')
+plt.show()
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+x = np.arange(-5.0, 5.0, 0.1)
+y1 = sigmoid(x+0.5)
+y2 = sigmoid(x+1)
+y3 = sigmoid(x+1.5)
+
+plt.plot(x, y1, 'r', linestyle='--') # x + 0.5
+plt.plot(x, y2, 'g') # x + 1
+plt.plot(x, y3, 'b', linestyle='--') # x + 1.5
+plt.plot([0,0],[1.0,0.0], ':') # 가운데 점선 추가
+plt.title('Sigmoid Function')
+plt.show()
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import optimizers
+
+X = np.array([-50, -40, -30, -20, -10, -5, 0, 5, 10, 20, 30, 40, 50])
+y = np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
+
+model = Sequential()
+model.add(Dense(1, input_dim =1, activation = 'sigmoid'))
+sgd = optimizers.SGD(lr = 0.01)
+
+model.compile(optimizer = sgd, loss = 'binary_crossentropy')
+
+model.fit(X, y, batch_size = 1, epochs = 300, shuffle = False)
+
+print(model.predict([1, 2, 3, 4, 4.5]))
+print(model.predict([11, 21, 31, 41, 500]))
